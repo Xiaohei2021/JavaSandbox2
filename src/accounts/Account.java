e10dd487894a4b77dea7448efc353b136d8b8f42
Leon <hualiang.tian@gmail.com>
2022-05-12 21:10:56 -0400

    testing out java class ineritance

---
+++ src/accounts/Account.java
@@ -0,0 +1,11 @@
+package accounts;
+
+public class Account {
+    private double balance;
+    private double interestRate;
+
+    public Account(double balance, double interestRate) {
+        this.balance = balance;
+        this.interestRate = interestRate;
+    }
+}
