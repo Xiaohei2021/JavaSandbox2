ae715f0c87e333c7eeec59bc2634356f36d69eac
Leon <hualiang.tian@gmail.com>
2022-05-17 22:45:03 -0400

    tinkering with java ineritance, playing around with withdraw and deposit method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import accounts.Account;
+
 public class Main {
     public static void main(String[] args) {
+        Account account = new Account(100, 0.025);
+        account.status();
+        account.withdraw(5);
+        account.withdraw(60);
+        account.deposit(500);
+        account.status();
+
         System.out.println("Hello world!");
     }
 }
--- src/accounts/Account.java
+++ src/accounts/Account.java
@@ -8,4 +8,28 @@ public class Account {
         this.balance = balance;
         this.interestRate = interestRate;
     }
+
+    public boolean withdraw(double amount){
+        if(amount>balance){
+            return false;
+        }
+        balance -= amount;
+        return true;
+    }
+
+    public void deposit(double amount){
+        balance +=amount;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    public void status(){
+        System.out.printf("Balance: %.2f\n", balance);
+    }
 }
