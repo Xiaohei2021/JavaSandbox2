6403459145ea7c7319de3f42d8a95b2704f88d1a
Leon <hualiang.tian@gmail.com>
2022-05-17 23:29:25 -0400

    playing around with protected variables and testing inheritance for withdraw method for saving and check account

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import accounts.Account;
 import accounts.CheckingAccount;
+import accounts.SavingsAccount;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,14 +13,22 @@ public class Main {
 //        account.deposit(3);
 //        account.withdraw(136);
 //        account.status();
-        CheckingAccount account = new CheckingAccount(100, 0.023);
-        account.status();
-        account.withdraw(5);
-        account.withdraw(60);
-        account.deposit(500);
-        account.status();
-        account.deposit(3);
-        account.withdraw(136);
-        account.status();
+//        CheckingAccount account = new CheckingAccount(100, 0.023, 700);
+//        if (account.purchase(500)){
+//            System.out.println("Purchase succeeded");
+//        }else{
+//            System.out.println(account.getBalance());
+//            System.out.println("Insufficient fund, transaction failed");
+//        }
+//        System.out.println(account.getRewardPoints());
+
+        CheckingAccount ca = new CheckingAccount(1000, 0.0123, 500);
+        SavingsAccount sa = new SavingsAccount(1000, 0.0123);
+
+        ca.withdraw(500);
+        sa.withdraw(500);
+
+        ca.status();
+        sa.status();
     }
 }
--- src/accounts/CheckingAccount.java
+++ src/accounts/CheckingAccount.java
@@ -1,9 +1,25 @@
 package accounts;
 
 public class CheckingAccount extends Account {
-    public CheckingAccount(double balance, double interestRate){
+    public int rewardPoints;
+    public CheckingAccount(double balance, double interestRate, int rewardPoints){
 //        super(50,0.0125);
 //        System.out.println("CheckingAccount constructor");
         super(balance, interestRate);
+        this.rewardPoints = rewardPoints;
+    }
+
+    public boolean purchase(double cost){
+        if(cost > balance){
+            return false;
+        }
+
+        balance -= cost;
+        rewardPoints += cost*10;
+        return true;
+    }
+
+    public int getRewardPoints() {
+        return rewardPoints;
     }
 }
