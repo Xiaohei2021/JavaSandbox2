f8aa2fb63ccf177dff03c117b8920d94f5823752
Leon <hualiang.tian@gmail.com>
2022-05-18 22:44:37 -0400

    deciphering polymorphis, differentiating between dynamic dispatch and object type vs variable type

--- src/Main.java
+++ src/Main.java
@@ -29,16 +29,33 @@ public class Main {
 //        ca.status();
 //        sa.status();
 
-        SilverCheckingAccount silver = new SilverCheckingAccount(600, 0.236, 900);
-        GoldCheckingAccount gold = new GoldCheckingAccount(600, 0.236, 900);
-        DiamondCheckingAccount rock = new DiamondCheckingAccount(600, 0.236, 900);
+//        SilverCheckingAccount silver = new SilverCheckingAccount(600, 0.236, 900);
+//        GoldCheckingAccount gold = new GoldCheckingAccount(600, 0.236, 900);
+//        DiamondCheckingAccount rock = new DiamondCheckingAccount(600, 0.236, 900);
+//
+//        silver.purchase(500);
+//        gold.purchase(500);
+//        rock.purchase(500);
+//
+//        System.out.println(silver.getRewardPoints());
+//        System.out.println(gold.getRewardPoints());
+//        System.out.println(rock.getRewardPoints());
 
-        silver.purchase(500);
-        gold.purchase(500);
-        rock.purchase(500);
+        Account acc1 = new Account(600, 0.025);
+        // acc1 = variable, it is an Account type, and it is a new Account object. But it can hold object of any subtype.
+        CheckingAccount acc2 = new GoldCheckingAccount(500,0.168,3500);
+        // acc2 is a variable, its type is CheckingAccount, and it is a new GoldCheckingAccount object
+        Account acc3 = new SilverCheckingAccount(900, 0.123, 200);
+        // acc3 is variable, its type is Account, and it is a new GoldCheckingAccount object
+        Account acc4 = new DiamondCheckingAccount(6000,0.236, 9000);
+        Account acc5 = new SavingsAccount(500,0.123);
 
-        System.out.println(silver.getRewardPoints());
-        System.out.println(gold.getRewardPoints());
-        System.out.println(rock.getRewardPoints());
+
+//        account1.purchase(50);
+        // error here because java only looks at variable type to determine the available method for the variable.
+        acc5.withdraw(90);
+        //polymorphism-dynamic dispatch: a call to a method will be directed to underlying object's method.Java calls on object type, instead of variable type.
+
+
     }
 }
